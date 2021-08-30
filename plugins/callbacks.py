397977fb1e06d3e8b61204449192584278100690
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-08-30 09:44:33 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
+                    InlineKeyboardButton("How to Deploy?", url="https://t.me/worldmoviesaj"),
+                    InlineKeyboardButton("About Me", url="https://t.me/AJmovieLINKS")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/AJmovieLINKS"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/worldmoviesaj")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/worldmoviesaj")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
